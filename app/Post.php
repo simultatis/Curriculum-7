8c536e69e1159ea75439a3784c739f454d4edf4b
simultatis <100739211+simultatis@users.noreply.github.com>
2022-03-31 15:08:53 +0900

    修正版

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
- dev_basis0-2
     public function getPaginateBylimit(int $limit_count = 10)
 {
     return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
