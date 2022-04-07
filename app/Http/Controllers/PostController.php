b8beb60d104b46987bd0301fae343431cf871bd4
simultatis <tatsukifungi121@gmail.com>
2022-04-07 22:49:10 +0900

    カリキュラム8-5仮

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,4 +11,34 @@ class PostController extends Controller
     {
         return view('posts/index')->with(['posts' => $post->getPaginateBylimit()]);
     }
+
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+
+    public function create()
+    {
+        return view('posts/create');
+    }
+
+    public function store(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+        
+        return redirect('/posts/' . $post->id);
+    }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,12 +8,15 @@
     </head>
     <body>
         <h1>Blog Name</h1>
+        <p class='create'>[<a href='/posts/create'>create</a>]</p>
         <div class='posts'>
             @foreach ($posts as $post)
-            <div class='post'>
-                <h2 class='title'>{{ $post->Title }}</h2>
-                <p class='body'>{{ $post->body }}</p>
-            </div>
+                <div class='post'>
+                    <h2 class='title'>
+                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                    </h2>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
             @endforeach
         </div>
         <div class='paginate'>
